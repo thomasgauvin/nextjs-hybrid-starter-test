37de43a7056c343e74e02a9396306336bb1ef8c1
annikel <92825476+annikel@users.noreply.github.com>
2024-03-07 12:12:51 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+    output: 'standalone',
 };
+
+module.exports = {
+distDir: 'build',
+}
 
 export default nextConfig;
